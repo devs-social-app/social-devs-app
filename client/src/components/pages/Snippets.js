6f77a6687e445c71e909ddbea2b822782f00d310
AbsolofDarkness <nicpapa98@gmail.com>
2019-03-23 11:25:51 -0500

    made snippets pretty

--- client/src/components/pages/Snippets.js
+++ client/src/components/pages/Snippets.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import { Pre, LineNo } from './styles'
-import Highlight, { defaultProps } from 'prism-react-renderer'
-import theme from 'prism-react-renderer/themes/nightOwl'
-import './style.css'
+import Highlight, { defaultProps } from "prism-react-renderer";
+import theme from "prism-react-renderer/themes/nightOwl";
+import React from "react";
+import "./style.css";
+import { LineNo, Pre } from "./styles";
 const exampleCode = `
 (function someDemo() {
   var test = "Hello World!";
@@ -10,53 +10,79 @@ const exampleCode = `
 })();
 
 return () => <App />;
-`.trim()
+`.trim();
 export default function Snippets() {
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-8">
-                    <article className="media content-section">
-                        <div className="media-body">
-                            <div className="article-metadata">
-                                <img src='favicon.ico' alt="alaa" className="rounded-circle article-img" />
-                                <a className="mr-2" href="/">Alaa Al Shammari</a>
-                                <small className="text-muted mr-2">Mar,22 2019</small>
-                            </div>
-                            <h2><a className="article-title" href="/">JAVASCRIPT SNIPPET</a></h2>
-                            <p className="desc">
-                                printing out hello world
-            </p>
-                            <Highlight {...defaultProps} theme={theme} code={exampleCode} language="jsx">
-                                {({ className, style, tokens, getLineProps, getTokenProps }) => (
-                                    <Pre className={className} style={style}>
-                                        {tokens.map((line, i) => (
-                                            <div {...getLineProps({ line, key: i })}>
-                                                <LineNo>{i + 1}</LineNo>
-                                                {line.map((token, key) => <span {...getTokenProps({ token, key })} />)}
-                                            </div>
-                                        ))}
-                                    </Pre>
-                                )}
-                            </Highlight>
-                            <small className="text-muted md-3">written with : javascript</small>
-
-                        </div>
-                    </article>
-                </div>
-                <div className="col-md-4">
-                    <div className="content-section">
-                        <h3>Snippets that matter</h3>
-                        <p className='text-muted'>all the lectures for this bootcamp</p>
-                           <ul className="list-group">
-                                <li className="list-group-item list-group-item-light">Latest Posts</li>
-                                <li className="list-group-item list-group-item-light">JAVASCRIPT</li>
-                                <li className="list-group-item list-group-item-light">CSS</li>
-                                <li className="list-group-item list-group-item-light">HTML</li>
-                            </ul>
-                    </div>
-                </div>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-8">
+          <article className="media content-section">
+            <div className="media-body">
+              <div className="article-metadata">
+                <img
+                  src="favicon.ico"
+                  alt="alaa"
+                  className="rounded-circle article-img"
+                />
+                <a className="mr-2" href="/">
+                  Alaa Al Shammari
+                </a>
+                <small className="text-muted mr-2">Mar,22 2019</small>
+              </div>
+              <h2>
+                <a className="article-title" href="/">
+                  JAVASCRIPT SNIPPET
+                </a>
+              </h2>
+              <p className="desc">printing out hello world</p>
+              <Highlight
+                {...defaultProps}
+                theme={theme}
+                code={exampleCode}
+                language="jsx"
+              >
+                {({
+                  className,
+                  style,
+                  tokens,
+                  getLineProps,
+                  getTokenProps
+                }) => (
+                  <Pre className={className} style={style}>
+                    {tokens.map((line, i) => (
+                      <div {...getLineProps({ line, key: i })}>
+                        <LineNo>{i + 1}</LineNo>
+                        {line.map((token, key) => (
+                          <span {...getTokenProps({ token, key })} />
+                        ))}
+                      </div>
+                    ))}
+                  </Pre>
+                )}
+              </Highlight>
+              <small className="text-muted md-3">
+                written with : javascript
+              </small>
             </div>
+          </article>
         </div>
-    )
+        <div className="col-md-4">
+          <div className="content-section">
+            <h3>Snippets that matter</h3>
+            <p className="text-muted">all the lectures for this bootcamp</p>
+            <ul className="list-group">
+              <li className="list-group-item list-group-item-light">
+                Latest Posts
+              </li>
+              <li className="list-group-item list-group-item-light">
+                JAVASCRIPT
+              </li>
+              <li className="list-group-item list-group-item-light">CSS</li>
+              <li className="list-group-item list-group-item-light">HTML</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
